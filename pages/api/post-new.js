8ea4a7630fa544c317217f5840529f85eae5d903
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-10-20 16:06:31 -0300

    test locale

--- pages/api/post-new.js
+++ pages/api/post-new.js
@@ -1,6 +1,7 @@
 import { GoogleSpreadsheet } from "google-spreadsheet"
 import { fromBase64 } from "../../lib/base64"
-import moment from "moment";
+import * as moment from 'moment';
+import 'moment/locale/pt-br';
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
@@ -21,7 +22,7 @@ export default async (req, res) => {
     Categoria: data.Categoria,
     Atuacao: data.Atuacao,
     Instagram: data.Instagram,
-    "Data Preenchimento": moment().format("DD/MM/YYYY, HH:mm:ss").locale('pt-br'),
+    "Data Preenchimento": moment().locale('pt-br').format("DD/MM/YYYY, HH:mm:ss"),
   })
 
   res.json({ status: true })
